e50f1d8b27d3d0f35577fa3764a4d32bd26ec8b5
Matias Mangiaterra <100295991+MottaMangiaterra@users.noreply.github.com>
2023-06-02 09:47:26 -0300

    Delete Cinee directory

--- Cinee/src/CINE/SistemaVirtual.java
+++
@@ -1,5 +0,0 @@
-package CINE;
-
-public class SistemaVirtual {
-
-}
--- Cinee/src/Main.java
+++
@@ -1,8 +0,0 @@
-import CINE.SistemaVirtual;
-import CINE.Peliculaa;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
